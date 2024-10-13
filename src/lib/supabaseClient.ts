8fe15f11d39042a049915a3e43fdcc29d02e4508
vbreddyobulareddy <vbreddy.obulareddy@gmail.com>
2024-10-13 17:57:39 -0400

    updated Supabase details with .env

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,3 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient('https://hzlxtcndukixsjucliuu.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh6bHh0Y25kdWtpeHNqdWNsaXV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjgyMzIyMDAsImV4cCI6MjA0MzgwODIwMH0.koFAY-O-Hxh2UZYMjQatDqmFbdgx-RUKaF27E8JA3kM')
+export const supabase = createClient(
+  import.meta.env.VITE_project,
+  import.meta.env.VITE_anon
+);
